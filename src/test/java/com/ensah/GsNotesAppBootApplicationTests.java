74270c17a6e5507bb8e9f13f21bba38eb0870d59
Fatima <azahrioufatma@gmail.com>
2022-11-26 11:10:26 +0100

    commit

---
+++ src/test/java/com/ensah/GsNotesAppBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ensah;
+
+import com.gsnotes.ApplicationLauncher;
+import org.apache.poi.sl.usermodel.ObjectMetaData;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest(classes = ApplicationLauncher.class)
+class GsNotesAppBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
